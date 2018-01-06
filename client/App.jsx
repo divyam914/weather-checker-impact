f6f41269f80cf1fda107aae347d5047ca5b8d830
Ricardo Nunes <rds493@nyu.edu>
2018-01-06 16:07:47 -0500

    With zip prompt

--- client/App.jsx
+++ client/App.jsx
@@ -4,24 +4,15 @@ import ZipForm from './ZipForm.jsx';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      zip = 00000
-    }
-
-    this.updateZip = this.updateZip.bind(this);
+    console.log("HERE");
   }
 
-  updateZip(zipcode) {
-    this.setState({
-      zip: zipcode;
-    })
-
-  }
   render() {
+    console.log("RENDER APP");
     return (
       <div className="container">
         <h1>What's the weather?</h1>
-        <ZipForm/>
+        <ZipForm />
       </div>
     )
   }
--- client/ZipForm.jsx
+++ client/ZipForm.jsx
@@ -3,14 +3,26 @@ import React from 'react';
 class ZipForm extends React.Component{
   constructor() {
     super();
+    this.state =
+      {
+          zipcode: 0
+      }
+      this.updateZip = this.updateZip.bind(this);
+  }
+
+  updateZip(zipCode){
+      this.setState({zip:zipCode});
   }
 
   render() {
-    return (
-      <div id="zip-form">
-      <input placeholder='Enter your Zip Code' />
-      <button>Go</button>
+      console.log("RENDER ZIP");
+      return (
+        <div id="zip-form">
+        <input placeholder='Enter your Zip Code' />
+        <button>Go</button>
       </div>
     )
   }
 }
+
+export default ZipForm;
